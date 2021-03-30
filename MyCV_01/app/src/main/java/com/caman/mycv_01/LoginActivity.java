59eb1703933545492010275747e425d64b9b4b34
Hafiz Shahman <hafizshahman1@gmail.com>
2021-03-30 15:00:40 +0800

    .

--- MyCV_01/app/src/main/java/com/caman/mycv_01/LoginActivity.java
+++ MyCV_01/app/src/main/java/com/caman/mycv_01/LoginActivity.java
@@ -1,8 +1,27 @@
 package com.caman.mycv_01;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
 
